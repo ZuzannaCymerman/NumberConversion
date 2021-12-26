8462efd90cb5d1bdb9a3a73e80870c4cdd491ba0
Zuzanna Cymerman <zuzannacymerman@zuzannas-macbook-air.home>
2021-12-26 22:18:37 +0100

    Add nww

--- nww/nww.cpp
+++ nww/nww.cpp
@@ -6,17 +6,21 @@ using namespace std;
 vector<string> divideString(string input, char *separator);
 int n;
 int *numbers;
+int product = 1;
+int nww;
 
 int main()
 {
 
-    //cin >> n;
-    n = 3;
+    string in;
+    getline(cin, in);
+    n = stoi(in);
+
     numbers = new int[n];
 
-    //string input;
-    //cin >> input;
-    string input = "1 2 3";
+    string input;
+    getline(cin, input);
+
     char separator[] = " ";
 
     vector<string> inputVector = divideString(input, separator);
@@ -26,10 +30,34 @@ int main()
         numbers[i] = stoi(inputVector[i]);
     }
 
+    for (int i = 0; i < n; i++)
+    {
+        product = product * numbers[i];
+    }
+
+    for (int i = 1; i <= product; i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < n; j++)
+        {
+            if (i % numbers[j] == 0)
+            {
+                sum++;
+            }
+        }
+
+        if (sum == n)
+        {
+            nww = i;
+            break;
+        }
+    }
+    cout << nww;
     return 0;
 }
 
-vector<string> divideString(string input, char *separator)
+vector<string> divideString(string input, char separator[])
 {
     char inputCharArray[input.size()];
     strcpy(inputCharArray, input.c_str());
